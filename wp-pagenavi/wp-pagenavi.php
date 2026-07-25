93aad1b9bf970c395a135b051f8981932df09726
Sean Murphy <sean@iamseanmurphy.com>
2026-07-25 03:33:08 +0000

    2.94.6

--- wp-pagenavi/wp-pagenavi.php
+++ wp-pagenavi/wp-pagenavi.php
@@ -3,10 +3,28 @@
 Plugin Name: WP-PageNavi
 Plugin URI: https://lesterchan.net/portfolio/programming/php/
 Description: Adds a more advanced paging navigation to your WordPress blog
-Version: 2.94.5
+Version: 2.94.6
 Author: Lester 'GaMerZ' Chan
 Author URI: https://lesterchan.net
 Text Domain: wp-pagenavi
+*/
+
+/*
+	Copyright 2026  Lester Chan  (email : lesterchan@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 require_once __DIR__ . '/scb/load.php';
